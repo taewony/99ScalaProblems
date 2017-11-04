622dee680b42bcda2b56f8361bbf73717804691a
taewony <engchat@gmail.com>
2017-11-04 22:30:48 +0900

    P04 commit

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,22 +5,31 @@ object Main {
   }
 
   /**
-  P01: Find the last element of a list 
-  */
-  def P01[T] (xs: List[T]): T = {
+   last: Find the last element of a list 
+   */
+  def last[T] (xs: List[T]): T = {
     xs.size match {
       case 1 => xs.head
-      case _ => P01(xs.tail)
+      case _ => last(xs.tail)
     }
   }
 
   /**
-  P02: Find the last but one element of a list 
-  */
-  def P02[T] (xs: List[T]): T = {
+   penultimate: Find the last but one element of a list 
+   */
+  def penultimate[T] (xs: List[T]): T = {
     xs.size match {
       case 2 => xs.head
-      case _ => last(xs.tail)
+      case _ => penultimate(xs.tail)
     }
   }
+
+  /**
+   nth: Find the Kth element of a list.
+   */
+  def nth[T] (n: Int, xs: List[T]): T =
+    if (n >= 0) xs(n)
+    else throw new NoSuchElementException
+
+  def length[T] (xs: List[T]): Int = xs.size
 }
--- src/test/scala/Test1.scala
+++ src/test/scala/Test1.scala
@@ -5,12 +5,19 @@ class Test1 {
   val x = List(1,2,3,4,5)
 
   @Test def t1(): Unit = {
-    assertEquals(Main.P01(x), 5) //P01: Find the last element of a list 
+    assertEquals(Main.last(x), 5) //P01: Find the last element of a list 
   }
 
   @Test def t2(): Unit = {
-    assertEquals(Main.P02(x), 4) //P02: Find the last but one element of a list 
+    assertEquals(Main.penultimate(x), 4) //P02: Find the last but one element of a list 
   }
 
+  @Test def t3(): Unit = {
+    assertEquals(Main.nth(2, x), 3) //P03: Find the Kth element of a list.
+  }
+
+  @Test def t4(): Unit = {
+    assertEquals(Main.length(x), 5) //P04: Find the number of elements of a list.
+  }
 
 }
